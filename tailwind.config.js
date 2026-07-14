64255f16a3a09801fd854f0c954e1bb0062089e9
Matt Robinson <mathurin_robinson@ymail.com>
2026-07-14 16:48:56 -0400

    vit update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/art-haus-shan/',
-})
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
